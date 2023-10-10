83768cf2e00c04184a4081c7447d1fb276620f32
Miri <mirifurman@gmail.com>
2023-10-10 12:27:01 +0300

    left sidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
 function LeftSidebar() {
   return (
-    <div>LeftSidebar</div>
-  )
+    <div className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link:any) => (
+          <Link href={link.route} key={link.label} className="leftsidebar_link">
+            <Image src={link.imgURL} alt={link.label} width={24} height={24} />
+
+            <p className="text-light-1 max-lg:hidden">{link.label}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default LeftSidebar
+export default LeftSidebar;
