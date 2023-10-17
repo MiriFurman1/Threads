a00a3016f5b27537bc1324f057531b4ea93a09cd
Miri <mirifurman@gmail.com>
2023-10-17 16:08:10 +0300

    upload image

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,7 +1,22 @@
 "use client";
-import {useForm} from 'react-hook-form'
-import { Form } from "@/components/ui/form";
-import {zodResolver} from '@hookform/resolvers/zod'
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { UserValidation } from "@/lib/validations/user";
+import { Button } from "@/components/ui/button";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import {ChangeEvent} from 'react'
+
 interface Props {
   user: {
     id: string;
@@ -14,17 +29,80 @@ interface Props {
   btnTitle: string;
 }
 function AccountProfile({ user, btnTitle }: Props) {
-    const form=useForm({
-        resolver: zodResolver()
-    })
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: "",
+      name: "",
+      username: "",
+      bio: "",
+    },
+  });
+
+const handleImage =(e:ChangeEvent)=>{
+    e.preventDefault()
+}
+
+  function onSubmit(values: z.infer<typeof UserValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+
   return (
-  <div>
-    <Form>
-
-    </Form>
-
-  </div>
-  )
+    <div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col justify-start gap-10"
+        >
+          <FormField
+            control={form.control}
+            name="profile_photo"
+            render={({ field }) => (
+              <FormItem className="flex items-center gap-4">
+                <FormLabel className="account-form_image-label">
+                  {field.value ? (
+                    <Image
+                      src={field.value}
+                      alt="profile photo"
+                      width={96}
+                      height={96}
+                      priority
+                      className="rounded-full object-contain"
+                    />
+                  ) : (
+                    <Image
+                      src="/assets/profile.svg"
+                      alt="profile photo"
+                      width={24}
+                      height={24}
+                      className="rounded-full object-contain"
+                    />
+                  )}
+                </FormLabel>
+                <FormControl className="flex-1 text=base-semibold text-gray-200">
+                  <Input
+                    type="file"
+                    accept="image/*"
+                    placeholder="Upload a photo"
+                    className="account-form_image-input"
+                    onChange={(e)=> handleImage(e, field.onChange)}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
 
 export default AccountProfile;
