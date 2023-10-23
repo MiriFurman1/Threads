50e1a23d8d35ff848aab74364576cf1ccc7a06ae
Miri <mirifurman@gmail.com>
2023-10-23 14:33:19 +0300

    onboarding complete

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -16,7 +16,9 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import { isBase64Image } from "@/lib/utils";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
   user: {
@@ -30,25 +32,52 @@ interface Props {
   btnTitle: string;
 }
 function AccountProfile({ user, btnTitle }: Props) {
+  const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   });
 
   const handleImage = (
-    e: ChangeEvent,
+    e: ChangeEvent<HTMLInputElement>,
     fieldChange: (value: string) => void
   ) => {
     e.preventDefault();
+    const fileReader = new FileReader();
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+
+      setFiles(Array.from(e.target.files));
+
+      if (!file.type.includes("image")) return;
+
+      fileReader.onload = async (event) => {
+        const imageDataUrl = event.target?.result?.toString() || "";
+
+        fieldChange(imageDataUrl);
+      };
+      fileReader.readAsDataURL(file);
+    }
   };
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    console.log(values);
+  const onSubmit= async (values: z.infer<typeof UserValidation>) =>{
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+      if(imgRes && imgRes[0].fileUrl){
+        values.profile_photo=imgRes[0].fileUrl;
+      }
+    }
+
+    //TODO: update the user profile in the db
   }
 
   return (
@@ -144,7 +173,7 @@ function AccountProfile({ user, btnTitle }: Props) {
                 </FormLabel>
                 <FormControl className="flex-1 text=base-semibold text-gray-200 bg-dark-1 border-none">
                   <Textarea
-                    rows={10}
+                    rows={7}
                     type="text"
                     className="account-form-input no-focus"
                     {...field}
@@ -154,7 +183,9 @@ function AccountProfile({ user, btnTitle }: Props) {
             )}
           />
 
-          <Button type="submit" className="bg-primary-500">Submit</Button>
+          <Button type="submit" className="bg-primary-500">
+            Submit
+          </Button>
         </form>
       </Form>
     </div>
